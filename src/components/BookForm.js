c4a3c7fe0a0e502698330aa9b86f2b560249ecac
PrangonGhose <prangonghose@gmail.com>
2023-01-28 16:23:17 +0600

    Add form

---
+++ src/components/BookForm.js
@@ -0,0 +1,32 @@
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react';
+
+const BookForm = () => {
+  const [inputText, setInputText] = useState({
+    title: '',
+  });
+
+  const onChange = (e) => {
+    setInputText({
+      ...inputText,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <div className="form">
+      <h1 className="newBook">ADD NEW BOOK</h1>
+      <input type="text" name="bookTitle" onChange={onChange} required placeholder="Book Title" />
+      <input type="text" name="bookAuthor" onChange={onChange} required placeholder="Name of the Author" />
+      <select name="bookGenre" className="bookGenreList">
+        <option value="Action">Action</option>
+        <option value="Science Fiction">Science Fiction</option>
+        <option value="Economy">Economy</option>
+        <option value="Others">Others</option>
+      </select>
+      <button type="submit" className="bookAddButton">Add Book</button>
+    </div>
+  );
+};
+
+export default BookForm;
