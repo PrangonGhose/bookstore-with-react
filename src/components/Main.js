681fd5837b02d1b6378e2aee84eaad772ebe0c49
PrangonGhose <prangonghose@gmail.com>
2023-02-03 04:35:18 +0600

    Add store provider

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,52 +1,26 @@
-/* eslint-disable react/prop-types */
-import React, { useState } from 'react';
+import React from 'react';
 import { Link, Routes, Route } from 'react-router-dom';
 import Booklist from './Booklist';
 import Categories from './Categories';
 
-const Main = () => {
-  const [bookList, setBookList] = useState({ // eslint-disable-line
-    bookList: [
-      {
-        bookId: 1,
-        bookGenre: 'Action',
-        bookName: 'The Hunger Games',
-        bookAuthor: 'Suzanne Collins',
-      },
-      {
-        bookId: 2,
-        bookGenre: 'Science Fiction',
-        bookName: 'Dune',
-        bookAuthor: 'Frank Herbert',
-      },
-      {
-        bookId: 3,
-        bookGenre: 'Economy',
-        bookName: 'Capital in the Twenty-First Century',
-        bookAuthor: 'Suzanne Collins',
-      },
-    ],
-  });
-
-  return (
-    <div className="main-container">
-      <header>
-        <h1>
-          <Link to="/" className="logo">Bookstore CMS</Link>
-        </h1>
-        <nav>
-          <ul>
-            <li><Link to="/" className="link">Books</Link></li>
-            <li><Link to="/categories" className="link">Categories</Link></li>
-          </ul>
-        </nav>
-      </header>
-      <Routes>
-        <Route path="/" exact element={<Booklist bookList={bookList} />} />
-        <Route path="/categories" exact element={<Categories />} />
-      </Routes>
-    </div>
-  );
-};
+const Main = () => (
+  <div className="main-container">
+    <header>
+      <h1>
+        <Link to="/" className="logo">Bookstore CMS</Link>
+      </h1>
+      <nav>
+        <ul>
+          <li><Link to="/" className="link">Books</Link></li>
+          <li><Link to="/categories" className="link">Categories</Link></li>
+        </ul>
+      </nav>
+    </header>
+    <Routes>
+      <Route path="/" exact element={<Booklist />} />
+      <Route path="/categories" exact element={<Categories />} />
+    </Routes>
+  </div>
+);
 
 export default Main;
