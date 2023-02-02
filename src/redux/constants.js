5d092330c14c4bbe60e0654bdf68aec4d951e6c4
PrangonGhose <prangonghose@gmail.com>
2023-02-03 04:35:33 +0600

    Update to use ducks pattern

--- src/redux/constants.js
+++ src/redux/constants.js
@@ -1,3 +1,3 @@
-export const ADD_BOOK = 'ADD_BOOK';
-export const REMOVE_BOOK = 'REMOVE_BOOK';
-export const CHECK_BOOK_CATEGORY = 'CHECK_BOOK_CATEGORY';
+export const ADD_BOOK = 'bookstore/src/redux/books/ADD_BOOK';
+export const REMOVE_BOOK = 'bookstore/src/redux/books/REMOVE_BOOK';
+export const CHECK_BOOK_CATEGORY = 'bookstore/src/redux/categories/CHECK_BOOK_CATEGORY';
