d70a8d92842c18dd8aa6fabb9458578e19e3c8aa
PrangonGhose <prangonghose@gmail.com>
2023-02-03 04:35:04 +0600

    Update Categories page

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,9 +1,18 @@
-/* eslint-disable */
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkBookCategory } from '../redux/categories/categories';
 
 const Categories = () => {
+  const category = useSelector((state) => state.categories);
+  const dispatch = useDispatch();
+
+  const checkCategory = () => dispatch(checkBookCategory());
+
   return (
-    <button type="submit">Check Status</button>
+    <div>
+      <h1>{category}</h1>
+      <button type="button" onClick={checkCategory}>Check Status</button>
+    </div>
   );
 };
 
