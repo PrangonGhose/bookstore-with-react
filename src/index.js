5dcb12a82b09a3757cf9ab56d07dd28ef4daffe9
PrangonGhose <prangonghose@gmail.com>
2023-02-02 22:53:06 +0600

    Add redux provider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Main from './components/Main';
+import store from './redux/configureStore';
 import './style.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <Main />
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <Main />
+      </HashRouter>
+    </Provider>
   </React.StrictMode>,
 );
