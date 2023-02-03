3daedc0ab82f342f6ab4b1cf839459542f4d09e2
PrangonGhose <prangonghose@gmail.com>
2023-02-04 02:08:49 +0600

    Update to use API

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,14 +1,29 @@
-import { ADD_BOOK, REMOVE_BOOK } from '../constants';
+import { ADD_BOOK, ALL_BOOKS, REMOVE_BOOK } from '../constants';
+import { postBooks, deleteBook, getBooks } from '../../components/connectAPI';
 
-export const AddBook = (data) => ({
-  type: ADD_BOOK,
-  data,
-});
+export const AddBook = (data) => async (dispatch) => {
+  postBooks(data);
+  dispatch({
+    type: ADD_BOOK,
+    data,
+  });
+};
 
-export const RemoveBook = (bookId) => ({
-  type: REMOVE_BOOK,
-  data: bookId,
-});
+export const RemoveBook = (bookId) => (dispatch) => {
+  deleteBook(bookId);
+  dispatch({
+    type: REMOVE_BOOK,
+    data: bookId,
+  });
+};
+
+export const GetBooks = () => async (dispatch) => {
+  const allBooks = await getBooks();
+  dispatch({
+    type: ALL_BOOKS,
+    data: allBooks,
+  });
+};
 
 const bookReducer = (state = [], action) => {
   switch (action.type) {
@@ -19,6 +34,8 @@ const bookReducer = (state = [], action) => {
       ];
     case REMOVE_BOOK:
       return state.filter((book) => book.bookId !== action.data);
+    case ALL_BOOKS:
+      return action.data;
     default:
       return state;
   }
