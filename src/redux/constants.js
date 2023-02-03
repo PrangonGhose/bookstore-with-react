b5ac7fd74965b1a5a9bb8268973fafac9971c98d
PrangonGhose <prangonghose@gmail.com>
2023-02-04 02:08:40 +0600

    Add ducks pattern action types

--- src/redux/constants.js
+++ src/redux/constants.js
@@ -1,3 +1,4 @@
 export const ADD_BOOK = 'bookstore/src/redux/books/ADD_BOOK';
 export const REMOVE_BOOK = 'bookstore/src/redux/books/REMOVE_BOOK';
+export const ALL_BOOKS = 'bookstore/src/redux/books/ALL_BOOKS';
 export const CHECK_BOOK_CATEGORY = 'bookstore/src/redux/categories/CHECK_BOOK_CATEGORY';
